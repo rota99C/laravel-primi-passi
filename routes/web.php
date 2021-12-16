7c9400c7fb81861152dc8f3c1e99030ccf5d2944
rota99C <rotaboolean@gmail.com>
2021-12-16 22:29:59 +0100

    l'esercizio è completo senza bonus e allego inoltre gli appunti nella cratella : views/appunti... presi guardando la registrazione

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data=[
+        "NAME" => 'Chandra',
+        "SURNAME"=>'Rota',
+        "AGE"=>'22',
+        "JOB"=>'Student'
+    ];
+    return view('home', $data);
 });
